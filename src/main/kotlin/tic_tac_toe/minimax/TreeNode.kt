34002db85463cfcfa904d5925fd3e01940c70a06
xray <creator@darkmattercreations.com>
2019-06-12 09:35:08 -0500

    [TWEAK] Refactoring TreeNode to reduce exposure of internal data structures

--- src/main/kotlin/tic_tac_toe/minimax/TreeNode.kt
+++ src/main/kotlin/tic_tac_toe/minimax/TreeNode.kt
@@ -1,8 +1,113 @@
 package tic_tac_toe.minimax
 
-class TreeNode(val value: Int, val children: Array<TreeNode>, val hasValue: Boolean){
-    constructor(points: Int, children: Array<TreeNode>) : this(points, children, true)
-    constructor(points: Int) : this(points, arrayOf<TreeNode>(), true)
-    constructor(children: Array<TreeNode>) : this(0, children, false)
-    constructor() : this(0, arrayOf<TreeNode>(), false)
+class TreeNode : Collection<TreeNode> {
+    val hasValue: Boolean
+    val value: Int
+
+    private val children: List<TreeNode>
+
+    constructor(){
+        children = listOf()
+        hasValue = false
+        value = 0
+    }
+
+    constructor(nodeValue: Int){
+        children = listOf()
+        hasValue = true
+        value = nodeValue
+    }
+
+    private constructor(newChildren: List<TreeNode>, newHasValue: Boolean, newValue: Int){
+        children = newChildren
+        hasValue = newHasValue
+        value = newValue
+    }
+
+    fun addChild(newChild: TreeNode) : TreeNode{
+        return if (children.isNotEmpty()) {
+            val newChildren = children.toMutableList()
+            newChildren.add(newChild)
+
+            TreeNode(newChildren, hasValue, value)
+        } else {
+            TreeNode(listOf(newChild), hasValue, value)
+        }
+
+    }
+
+    override fun contains(element: TreeNode): Boolean {
+        for (child in children){
+            if (child == element){
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun containsAll(elements: Collection<TreeNode>): Boolean {
+        var doesContainAll = true
+
+        for(newTreeNode in elements){
+            if(!this.contains(newTreeNode)){
+                doesContainAll = false
+            }
+        }
+
+        return doesContainAll
+    }
+
+    override fun isEmpty(): Boolean {
+        return children.isEmpty()
+    }
+
+    override fun iterator(): Iterator<TreeNode> {
+        return children.iterator()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other !is TreeNode) false
+        else (children == other.children && hasValue == other.hasValue && value == other.value)
+    }
+
+    override fun toString(): String {
+        var finalString = "Node:\n"
+        var childCount = 1
+
+        if (hasValue){
+            finalString += "  Value: $value\n"
+        }
+
+        if (size > 0){
+            finalString += "  Children ($size):\n"
+
+            for (child in children){
+                val childLines = child.toString().split('\n')
+                finalString += "    #$childCount Child ${childLines[0]}\n"
+
+                for ((lineNumber, line) in childLines.withIndex()){
+                    if (lineNumber != 0 && lineNumber != childLines.size - 1) {
+                        finalString += "    $line\n"
+                    }
+                }
+                childCount++
+            }
+        }
+
+        return finalString
+    }
+
+    override fun hashCode(): Int {
+        var result = hasValue.hashCode()
+        result = 31 * result + value
+        result = 31 * result + children.hashCode()
+        return result
+    }
+
+    operator fun get(i: Int): TreeNode {
+        return children[i]
+    }
+
+    override val size: Int
+        get() = children.size
 }
--- src/test/kotlin/tic_tac_toe/minimax/TreeNodeTest.kt
+++ src/test/kotlin/tic_tac_toe/minimax/TreeNodeTest.kt
@@ -5,49 +5,189 @@ import kotlin.test.Test
 class TreeNodeTest {
     @Test fun testItCreatesANodeWithNoValue() {
         val node = TreeNode()
+
         assert(!node.hasValue)
     }
 
-    @Test fun testItCreatesANodeWithPoints() {
+    @Test fun testItReturnsANodeWithAValue() {
         val node = TreeNode(5)
+
         assert(node.hasValue)
         assert(node.value == 5)
     }
 
     @Test fun testItHasZeroChildrenByDefault() {
         val node = TreeNode()
-        assert(node.children.isEmpty())
+
+        assert(node.isEmpty())
     }
 
     @Test fun testItCanHaveOneChild() {
         val child = TreeNode(20)
-        val parent = TreeNode(40, arrayOf(child))
+        val parent = TreeNode(40).addChild(child)
 
-        assert(parent.children.contentEquals(arrayOf(child)))
+        assert(parent.contains(child))
     }
 
-    @Test fun testItCanHaveOneChildAndNoPoints() {
+    @Test fun testItCanHaveOneChildAndNoValue() {
         val child = TreeNode(10)
-        val parent = TreeNode(arrayOf(child))
+        val parent = TreeNode().addChild(child)
 
-        assert(parent.children.contentEquals(arrayOf(child)))
+        assert(!parent.hasValue)
+        assert(parent.contains(child))
     }
 
-    @Test fun testItCanHaveAParentWithThreeChildren() {
+    @Test fun testItReturnsANodeWithThreeChildren() {
         val child1 = TreeNode(10)
         val child2 = TreeNode(20)
         val child3 = TreeNode(30)
-        val parent = TreeNode(60, arrayOf(child1, child2, child3))
 
-        assert(parent.children.contentEquals(arrayOf(child1, child2, child3)))
+        val parent = TreeNode(60)
+                .addChild(child1)
+                .addChild(child2)
+                .addChild(child3)
+
+        assert(parent.contains(child1))
+        assert(parent.contains(child2))
+        assert(parent.contains(child3))
     }
 
-    @Test fun testItCanCreateALinkedList() {
+    @Test fun testItReturnsALinkedList() {
         val child = TreeNode(10)
-        val parent = TreeNode(20, arrayOf(child))
-        val grandparent = TreeNode(30, arrayOf(parent))
+        val parent = TreeNode(10).addChild(child)
+        val grandparent = TreeNode(20).addChild(parent)
 
-        assert(grandparent.children.contentEquals(arrayOf(parent)))
-        assert(parent.children.contentEquals(arrayOf(child)))
+        assert(grandparent.contains(parent))
+    }
+
+    @Test fun testContainsReturnsTrueWhenNodeIsChild() {
+        val child = TreeNode(10)
+        val parent = TreeNode().addChild(child)
+
+        assert(parent.contains(child))
+    }
+
+    @Test fun testContainsReturnsFalseWhenNodeIsNotChild() {
+        val unrelatedNode = TreeNode(10)
+        val node = TreeNode(20)
+
+        assert(!node.contains(unrelatedNode))
+    }
+
+    @Test fun testContainsAllReturnsTrueWhenAllNodesAreChildren() {
+        val child1 = TreeNode(10)
+        val child2 = TreeNode(20)
+        val child3 = TreeNode(30)
+
+        val parent = TreeNode()
+                .addChild(child1)
+                .addChild(child2)
+                .addChild(child3)
+
+        assert(parent.containsAll(listOf(child1, child2, child3)))
+    }
+
+    @Test fun testContainsAllReturnsFalseWhenNotAllNodesAreChildren() {
+        val child1 = TreeNode(10)
+        val child2 = TreeNode(20)
+        val child3 = TreeNode(30)
+        val unrelatedNode = TreeNode(45)
+
+        val parent = TreeNode()
+                .addChild(child1)
+                .addChild(child2)
+                .addChild(child3)
+
+        assert(!parent.containsAll(listOf(child1, child2, child3, unrelatedNode)))
+    }
+
+    @Test fun testIsEmptyReturnsTrueWhenNodeHasNoChildren() {
+        val childless = TreeNode(10)
+
+        assert(childless.isEmpty())
+    }
+
+    @Test fun testIsEmptyReturnsFalseWhenNodeHasChildren() {
+        val child = TreeNode(10)
+        val parent = TreeNode().addChild(child)
+
+        assert(!parent.isEmpty())
+    }
+
+    @Test fun testEqualsReturnsTrueWhenTheyAreTheSame() {
+        val foo = TreeNode(10)
+        val bar = TreeNode(10)
+
+        assert(foo == bar)
+        assert(foo.equals(bar))
+    }
+
+    @Test fun testEqualsReturnsFalseWhenTheyAreDifferent() {
+        val foo = TreeNode(10)
+        val bar = TreeNode(20)
+
+        assert(foo != bar)
+        assert(!foo.equals(bar))
+    }
+
+    @Test fun testEqualsReturnsFalseWhenTheyAreDifferentTypes(){
+        val foo = TreeNode(10)
+        val bar = 10
+
+        assert(!foo.equals(bar))
+    }
+
+    @Test fun testToStringReturnsOnlyValueWhenNoChildren() {
+        val node = TreeNode(10)
+        val expectedOutput = "Node:\n" +
+                             "  Value: 10\n"
+
+        assert(node.toString() == expectedOutput)
+    }
+
+    @Test fun testToStringReturnsOnlyChildrenWhenNoValue() {
+        val child = TreeNode(10)
+        val parent = TreeNode().addChild(child)
+        val expectedOutput = "Node:\n" +
+                             "  Children (1):\n" +
+                             "    #1 Child Node:\n" +
+                             "      Value: 10\n"
+
+        assert(parent.toString() == expectedOutput)
+    }
+
+    @Test fun testToStringReturnsMultipleChildren() {
+        val child1 = TreeNode(10)
+        val child2 = TreeNode(20)
+        val parent = TreeNode()
+                .addChild(child1)
+                .addChild(child2)
+
+        val expectedOutput = "Node:\n" +
+                             "  Children (2):\n" +
+                             "    #1 Child Node:\n" +
+                             "      Value: 10\n" +
+                             "    #2 Child Node:\n" +
+                             "      Value: 20\n"
+
+        assert(parent.toString() == expectedOutput)
+    }
+
+    @Test fun testItReturnsTheFirstChild() {
+        val child = TreeNode(10)
+        val parent = TreeNode().addChild(child)
+
+        assert(parent[0] == child)
+    }
+
+    @Test fun testItReturnsTheNumberOfChildren() {
+        val child1 = TreeNode(10)
+        val child2 = TreeNode(20)
+
+        val parent = TreeNode()
+                .addChild(child1)
+                .addChild(child2)
+
+        assert(parent.size == 2)
     }
 }
