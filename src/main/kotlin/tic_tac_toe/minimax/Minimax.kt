84eb9c707e50195651ba407ffefc19f4e409ed56
xray <creator@darkmattercreations.com>
2019-06-12 10:29:32 -0500

    [FEATURE] Using TreeNode to implement minimax algorithm

--- src/main/kotlin/tic_tac_toe/minimax/Minimax.kt
+++ src/main/kotlin/tic_tac_toe/minimax/Minimax.kt
@@ -1,5 +1,33 @@
 package tic_tac_toe.minimax
 
 fun score(node: TreeNode) : Int {
-    return node.value
+    return maximize(node)
+}
+
+private fun maximize(node: TreeNode) : Int{
+    return if (node.hasValue){
+        node.value
+    } else {
+        var childValues = arrayOf<Int>()
+
+        for (child in node) {
+            childValues += minimize(child)
+        }
+
+        childValues.max()!!
+    }
+}
+
+private fun minimize(node: TreeNode) : Int{
+    return if (node.hasValue){
+        node.value
+    } else {
+        var childValues = arrayOf<Int>()
+
+        for (child in node) {
+            childValues += maximize(child)
+        }
+
+        childValues.min()!!
+    }
 }
